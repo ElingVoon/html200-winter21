99a3f0272166eb6b894423b40464f775885453e3
ElingVoon <72539062+ElingVoon@users.noreply.github.com>
2021-03-15 11:04:03 -0700

    Add files via upload

--- Lesson-08/scripts/inClassExercise.js
+++ Lesson-08/scripts/inClassExercise.js
@@ -3,6 +3,10 @@ $('h1').addClass('large-headline');
 
 $('article').addClass('half-width');
 
-$('button').click(function (e) {
+$('button').click(function(){
 	$('img').toggleClass('image-border');
 });
+
+// $('button').click(function(){
+//   $('p').toggleClass('main');
+// });
