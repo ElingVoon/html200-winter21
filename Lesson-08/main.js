f3b0a4c33470d1bd8c4a8ca08f656a7031ff8e0b
ElingVoon <72539062+ElingVoon@users.noreply.github.com>
2021-03-14 20:04:59 -0700

    Add files via upload

---
+++ Lesson-08/main.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+// // console.log('does this work')
+// const form = document.createElement('form');
+// const fields = ['firstName', 'lastName'];
+//
+// for (let i=0,; i < fields.length i++) {
+// 	const label = document.createElement('label');
+// 	label.textContent = fields[i];
+// 	label.setAttribute('for')
+// }
---
+++ Lesson-08/scripts/inClassExercise.js
@@ -0,0 +1,8 @@
+
+$('h1').addClass('large-headline');
+
+$('article').addClass('half-width');
+
+$('button').click(function (e) {
+	$('img').toggleClass('image-border');
+});
